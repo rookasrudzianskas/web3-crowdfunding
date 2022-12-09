7b26b1750ff43195a0fa3f17272212d11e2cc541
Rokas <rookas.rudzenskas@gmail.com>
2022-12-09 16:45:52 +0200

    Tailwindcss

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,18 +1,15 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
 import App from "./App";
-import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
-import "./styles/globals.css";
 
-// This is the chainId your dApp will work on.
-const activeChainId = ChainId.Mainnet;
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const container = document.getElementById("root");
-const root = createRoot(container);
 root.render(
-  <React.StrictMode>
-    <ThirdwebProvider desiredChainId={activeChainId}>
-      <App />
+    <ThirdwebProvider desiredChainId={ChainId.Goerli}>
+        <Router>
+            <App />
+        </Router>
     </ThirdwebProvider>
-  </React.StrictMode>
-);
+    )
