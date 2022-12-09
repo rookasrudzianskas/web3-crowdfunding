b22c102f8de995caa49876489d51a50b92161bf6
Rokas <rookas.rudzenskas@gmail.com>
2022-12-09 20:12:57 +0200

    feat: add state context provider
    This commit adds a state context provider to the app.
    
    The provider exposes the following methods:
    
    - address: the address of the current user
    - connect: a function to connect to metamask
    - createCampaign: a function to create a new campaign

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -4,3 +4,35 @@ import { useAddress, useContract, useMetamask, useContractWrite } from '@thirdwe
 import { ethers } from 'ethers';
 import { EditionMetadataWithOwnerOutputSchema } from '@thirdweb-dev/sdk';
 
+const StateContext = createContext();
+
+export const StateContextProvider = ({ children }) => {
+    const { contract } = useContract("0x04af4F2d115af646AC1f8A1f0FBf5d79b8BBAa9F");
+    const { mutateAsync: createCampaign } = useContractWrite(contract, "createCampaign");
+
+    const address = useAddress();
+    const connect = useMetamask();
+
+    const publishCampaign = async (form) => {
+        try {
+            const data = await createCampaign([
+                address, // owner
+                form.title, // title
+                form.description, // description
+                form.target,
+                new Date(form.deadline).getTime(), // deadline,
+                form.image
+            ]);
+
+            console.log("contract call success", data);
+        } catch (error) {
+            console.log("contract call failure", error);
+        }
+    }
+
+    return (
+        <StateContext.Provider value={{ address, connect, createCampaign }}>
+            {children}
+        </StateContext.Provider>
+    )
+}
