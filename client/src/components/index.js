33dfdb2e613ebdc1cc59e095d069a0002a2b3758
Rokas <rookas.rudzenskas@gmail.com>
2022-12-09 21:13:18 +0200

    feat: add count box component
    - Add count box component to display campaign details
    - Add count box component to campaign details page

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,3 +3,4 @@ export { default as Navbar } from './Navbar';
 export { default as CustomButton } from './CustomButton';
 export { default as FormField } from './FormField';
 export { default as FundCard } from './FundCard';
+export { default as CountBox } from './CountBox';
