253e08b07b604449ba1dfb01e6d671d4c7cced33
Rokas <rookas.rudzenskas@gmail.com>
2022-12-09 20:51:18 +0200

    Add FundCard component
    This component is used to display the information about a single fund.
    
    It is used in the Home page and the Fund page.

--- client/src/components/FundCard.jsx
+++ client/src/components/FundCard.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { tagType, thirdweb } from '../assets';
 import { daysLeft } from '../utils';
 
-
 const FundCard = ({ owner, title, description, target, deadline, amountCollected, image, handleClick }) => {
     const remainingDays = daysLeft(deadline);
 
@@ -21,10 +20,27 @@ const FundCard = ({ owner, title, description, target, deadline, amountCollected
                     <h3 className="font-epilogue font-semibold text-[16px] text-white text-left leading-[26px] truncate">{title}</h3>
                     <p className="mt-[5px] font-epilogue font-normal text-[#808191] text-left leading-[18px] truncate">{description}</p>
                 </div>
+
+                <div className="flex justify-between flex-wrap mt-[15px] gap-2">
+                    <div className="flex flex-col">
+                        <h4 className="font-epilogue font-semibold text-[14px] text-[#b2b3bd] leading-[22px]">{amountCollected}</h4>
+                        <p className="mt-[3px] font-epilogue font-normal text-[12px] leading-[18px] text-[#808191] sm:max-w-[120px] truncate">Raised of {target}</p>
+                    </div>
+                    <div className="flex flex-col">
+                        <h4 className="font-epilogue font-semibold text-[14px] text-[#b2b3bd] leading-[22px]">{remainingDays}</h4>
+                        <p className="mt-[3px] font-epilogue font-normal text-[12px] leading-[18px] text-[#808191] sm:max-w-[120px] truncate">Days Left</p>
+                    </div>
+                </div>
+
+                <div className="flex items-center mt-[20px] gap-[12px]">
+                    <div className="w-[30px] h-[30px] rounded-full flex justify-center items-center bg-[#13131a]">
+                        <img src={'https://yt3.ggpht.com/IQ4OqurVrPmACaf3h5fgTcRInn6QoHz0xN4O5qzhuhY7UKgpDg2A4mGyhWW5vcaGSiVbf_FLdQ=s900-c-k-c0x00ffffff-no-rj'} alt="user" className="w-1/2 h-1/2 object-contain"/>
+                    </div>
+                    <p className="flex-1 font-epilogue font-normal text-[12px] text-[#808191] truncate">by <span className="text-[#b2b3bd]">{owner}</span></p>
+                </div>
             </div>
         </div>
-    );
-};
+    )
+}
 
-export default FundCard;
-// by Rokas with ❤️
+export default FundCard
