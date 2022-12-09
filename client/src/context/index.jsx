23739f17eb7160d3edff612591221a8c857730e9
Rokas <rookas.rudzenskas@gmail.com>
2022-12-09 20:08:01 +0200

    feat: add context
    This commit adds the context to the app.

---
+++ client/src/context/index.jsx
@@ -0,0 +1,6 @@
+import React, { useContext, createContext } from 'react';
+
+import { useAddress, useContract, useMetamask, useContractWrite } from '@thirdweb-dev/react';
+import { ethers } from 'ethers';
+import { EditionMetadataWithOwnerOutputSchema } from '@thirdweb-dev/sdk';
+
