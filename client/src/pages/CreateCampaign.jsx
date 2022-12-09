321f789068e34df15fcefe5ce2ce811e18a13b84
Rokas <rookas.rudzenskas@gmail.com>
2022-12-09 19:27:03 +0200

    Add a button to create a campaign
    This button will allow users to create a campaign.
    
    Closes #1

--- client/src/pages/CreateCampaign.jsx
+++ client/src/pages/CreateCampaign.jsx
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { ethers } from 'ethers';
+
+import { money } from '../assets';
+import { CustomButton } from '../components';
+import { checkIfImage } from '../utils';
 
 const CreateCampaign = ({}) => {
+    const navigate = useNavigate();
+    const [isLoading, setIsLoading] = useState(false);
+
+
     return (
         <div>
             <h1>Create Campaign</h1>
